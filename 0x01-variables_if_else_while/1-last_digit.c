620d9f225404f8873e320688ddb6bc7e8944a718
Amaka6774git <talk2faithgeorge11@gmail.com@gmail.com>
2022-10-16 11:26:07 -0700

    commit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,19 @@ int main(void)
 	int lidgit;
 
 	stand(time(0);
-			n = rand() - RAND - MAX / 2;
-			ldigit = n % 10;
-			if (ldigit < 5)
-			{
-			printf("last digit of %d is %d grater than 5\n", n, ldgit);
-			}
-			else if (ldigit == 0)
-			{
-			printf("last digit of %d is %d and is 0\n", ldigit);
-			}
-			else if (ldigit < 6 && ldigit != 0)
-			{
-			printf("last digit of %d is %d and id less than 6 and not 0\n", n ldigit);
-			return (0);
-			}
+	n = rand() - RAND - MAX / 2;
+	ldigit = n % 10;
+	if (ldigit < 5)
+	{
+		printf("last digit of %d is %d grater than 5\n", n, ldgit);
+	}
+		else if (ldigit == 0)
+	{
+		printf("last digit of %d is %d and is 0\n", ldigit);
+	}
+		else if (ldigit < 6 && ldigit != 0)
+	{
+		printf("last digit of %d is %d and id less than 6 and not 0\n", n ldigit);
+	}
+	return (0);
+}
